b9dfb1ff51f8068111b8b362d06bc5ad6b4ed54d
sekelsta <sekelsta@protonmail.com>
2023-04-23 16:13:43 -0400

    Implement equals to go with hashCode

--- src/engine/Pair.java
+++ src/engine/Pair.java
@@ -19,10 +19,28 @@ public class Pair<K,V> {
         return value;
     }
 
+    @Override
     public int hashCode() {
         return Objects.hash(key, value);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if (!(o instanceof Pair)) {
+            return false;
+        }
+        Pair other = (Pair)o;
+        return ((this.key == null && other.key == null) || (this.key != null && this.key.equals(other.key)))
+            && ((this.value == null && other.value == null) || (this.value != null && this.value.equals(other.value)));
+    }
+
+    @Override
     public String toString() {
         return "<%s, %s>".format(key.toString(), value.toString());
     }
