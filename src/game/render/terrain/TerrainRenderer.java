0a019f83e15c43db1419af4ce6641fbdb950c551
sekelsta <sekelsta@protonmail.com>
2023-04-25 10:15:00 -0400

    Reduce neighborhood size

--- src/game/render/terrain/TerrainRenderer.java
+++ src/game/render/terrain/TerrainRenderer.java
@@ -24,7 +24,7 @@ public class TerrainRenderer {
 
     protected HashMap<ChunkPos, TerrainMesh> meshes = new HashMap<>();
 
-    protected int neighborhood = 2 + numIterations;
+    protected int neighborhood = 1 + numIterations;
 
     MeshingThread meshingThread;
 
